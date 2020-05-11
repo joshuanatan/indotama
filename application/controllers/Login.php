4738b4e076657a3ba4148b8cabc2d27f5fe317f4
wivinadaicy <43501017+wivinadaicy@users.noreply.github.com>
2020-05-12 00:15:27 +0700

    reset password kelar

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -101,26 +101,61 @@ class Login extends CI_Controller {
 		$this->form_validation->set_rules("user_name","Username","required");
 
 		$user_name = $this->input->post("user_name");
-
+		echo $user_name;
 		if($this->form_validation->run()){
 			$where = array(
 				"user_status" => "AKTIF"
 			);
-			if(isExistsInTable("user",$where)){
+			if(isExistsInTable("mstr_user",$where)){
 				$where = array(
-					"emp_status" => "AKTIF"
+					"user_status" => "AKTIF",
+					"user_name" =>$user_name
 				);
-				if(isExistsInTable("employee",$where)){
-					$data['emp_email'] = get1Value("employee","emp_email",$where);
-
+				if(isExistsInTable("mstr_user",$where)){
+					$data['user_email'] = get1Value("mstr_user","user_email",$where);
+					$data['id_pk_user'] = get1Value("mstr_user","id_pk_user",$where);
+					echo $data['id_pk_user'];
 					//send email
-					//buat view cek email
+					$this->load->library('phpmailer_lib');
+					$mail = $this->phpmailer_lib->load();
+					$mail->IsSMTP();
+
+					$mail->Host       = "smtp.gmail.com";
+					$mail->SMTPSecure = "ssl";
+					$mail->Port       = 465;
+					$mail->Username   = "clickrentsistech@gmail.com";
+					$mail->Password   = "sistech123";
+
+					$mail->SMTPAuth   = true;
+					$mail->SetFrom('clickrentsistech@gmail.com', 'INDOTAMA');
+					$mail->AddAddress($data['user_email']);
+					$mail->Subject    = "Reset Your Password";
+					$mail->IsHTML(true);
+
+					$isiEmail = "<center style='color:black'>LOGO<br>
+					<h2>Reset Password</h2>
+					<p>Hi<br>Reset your password by clicking <a href='".base_url()."Login/forget_get_new_pass/".$data['id_pk_user']."'>HERE</a></center>
+					";
+					$mail->Body       = $isiEmail;
+		
+					//$mail->SMTPDebug = 2;
+
+					if(!$mail->Send()) {
+						//echo "Mailer Error: " . $mail->ErrorInfo;
+						echo "<script>alert('Gagal reset password! Silahkan hubungi admin sistem!'); window.location.href='../99dea78007133396a7b8ed70578ac6ae';</script>";
+					} 
+					else {
+						echo "<script>alert('Cek email Anda dan ikuti instruksi selanjutnya untuk melakukan reset password!'); window.location.href='../99dea78007133396a7b8ed70578ac6ae';</script>";
+					}
 					//buat view new password
 					//buat method update password dan ke login lagi
-					$this->load->view("V_forget_pw_sendmail");
+				}else{
+					$response["status"] = "ERROR";
+					$response["msg"] = "Employee tidak ditemukan!";
 				}
 			}else{
-
+				$response["status"] = "ERROR";
+				$response["msg"] = "Employee tidak ditemukan!";
 			}
 		}else{
 			$response["status"] = "ERROR";
@@ -128,5 +163,63 @@ class Login extends CI_Controller {
 			$this->session->set_flashdata("msg",$response['msg']);
 			redirect("login/forget_password");
 		}
+	}
+
+	public function forget_get_new_pass($id_pk_user){
+		$data['id_pk_user'] = $id_pk_user;
+		$this->load->view('V_reset_password',$data);
+	}
+
+	function cek_password(){
+        $passbaru = $this->input->post('passb');
+        $passkonfir = $this->input->post('passk');
+
+        if($passbaru != $passkonfir || $passbaru=="" || $passbaru=="" || strlen($passbaru)<9 || strlen($passbaru)<9){
+            echo "1";
+        }else{
+            echo "0";
+        }
+    }
+
+	function pass_reset(){
+        $config = array(
+            array(
+                "field" => "user_pass",
+                "label" => "Password",
+                "rules" => "required|min_length[8]"
+            ),
+        );
+        $this->form_validation->set_rules($config);
+        if($this->form_validation->run()){
+            $data = array(
+                "user_pass"=>password_hash($this->input->post('user_pass'),PASSWORD_DEFAULT),
+				"id_last_modified"=>$this->session->id_user,
+				"user_last_modified"=>date("Y-m-d H:i:s")
+            );
+            $where = array(
+                "id_pk_user"=>$this->input->post('id_pk_user')
+            );
+            updateRow("mstr_user",$data,$where);
+
+			echo "<script>alert('Password telah diperbarui! Silahkan login'); window.location.href='../99dea78007133396a7b8ed70578ac6ae';</script>";
+            
+        }
+        else{
+            $this->session->set_flashdata("status","danger");
+            $this->session->set_flashdata("msg",validation_errors());
+            redirect("login/forget_get_new_pass/" . $this->input->post('id_pk_user'));
+        }
+	}
+	
+	public function logout(){
+		$session = array(
+			"id_user",
+			"user_name",
+			"user_email",
+			"role",
+			"user_status"
+		);
+		$this->session->unset_userdata($session);
+		redirect(md5("Login"));
 	}
 }
