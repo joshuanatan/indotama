2bd49e4eebb919c2234a4a93f193390994a6e093
joshuanatan <joshuanatan.jn@gmail.com>
2020-05-13 22:02:27 +0700

    Update f-add-pembelian.php

--- application/views/pembelian/f-add-pembelian.php
+++ application/views/pembelian/f-add-pembelian.php
@@ -57,7 +57,7 @@
                     </div>
                     <div class = "form-group">
                         <button type = "button" class = "btn btn-sm btn-danger" data-dismiss = "modal">Cancel</button>
-                        <button type = "button" onclick = "register_func()" class = "btn btn-sm btn-primary">Submit</button>
+                        <button type = "button" onclick = "register_func();empty_table_form()" class = "btn btn-sm btn-primary">Submit</button>
                     </div>
                 </form>
             </div>
@@ -67,14 +67,20 @@
 <script>
     var brg_beli_row = 0;  
     function add_brg_beli_row(){
-        var html = "<tr><td id = 'row"+brg_beli_row+"'><input name = 'check[]' checked value = "+brg_beli_row+" type = 'hidden'><input type = 'text' list = 'daftar_barang' name = 'brg"+brg_beli_row+"' class = 'form-control'></td><td><input name = 'brg_qty"+brg_beli_row+"' type = 'text' class = 'form-control'></td><td><input type = 'text' name = 'brg_price"+brg_beli_row+"' class = 'form-control'></td><td><input type = 'text' name = 'brg_notes"+brg_beli_row+"' class = 'form-control'></td><td><i style = 'cursor:pointer;font-size:large;margin-left:10px' class = 'text-danger md-delete' onclick = '$(this).parent().parent().remove()'></i></td></tr>";
+        var html = "<tr class = 'add_brg_beli_row'><td id = 'row"+brg_beli_row+"'><input name = 'check[]' value = "+brg_beli_row+" type = 'hidden'><input type = 'text' list = 'daftar_barang' name = 'brg"+brg_beli_row+"' class = 'form-control'></td><td><input name = 'brg_qty"+brg_beli_row+"' type = 'text' class = 'form-control'></td><td><input type = 'text' name = 'brg_price"+brg_beli_row+"' class = 'form-control'></td><td><input type = 'text' name = 'brg_notes"+brg_beli_row+"' class = 'form-control'></td><td><i style = 'cursor:pointer;font-size:large;margin-left:10px' class = 'text-danger md-delete' onclick = '$(this).parent().parent().remove()'></i></td></tr>";
         $("#add_brg_beli_but_container").before(html);
         brg_beli_row++;    
     }
     var tambahan_beli_row = 0;
     function add_tambahan_beli_row(){
-        var html = "<tr><td><input name = 'tambahan[]' checked value = "+tambahan_beli_row+" type = 'hidden'><input name = 'tmbhn"+tambahan_beli_row+"' type = 'text' class = 'form-control'></td><td><input name = 'tmbhn_jumlah"+tambahan_beli_row+"' type = 'text' class = 'form-control'></td><td><input name = 'tmbhn_harga"+tambahan_beli_row+"' type = 'text' class = 'form-control'></td><td><input name = 'tmbhn_notes"+tambahan_beli_row+"' type = 'text' class = 'form-control'></td><td><i style = 'cursor:pointer;font-size:large;margin-left:10px' class = 'text-danger md-delete' onclick = '$(this).parent().parent().remove()'></i></td></tr>";
+        var html = "<tr class = 'add_tambahan_beli_row'><td><input name = 'tambahan[]' value = "+tambahan_beli_row+" type = 'hidden'><input name = 'tmbhn"+tambahan_beli_row+"' type = 'text' class = 'form-control'></td><td><input name = 'tmbhn_jumlah"+tambahan_beli_row+"' type = 'text' class = 'form-control'></td><td><input name = 'tmbhn_harga"+tambahan_beli_row+"' type = 'text' class = 'form-control'></td><td><input name = 'tmbhn_notes"+tambahan_beli_row+"' type = 'text' class = 'form-control'></td><td><i style = 'cursor:pointer;font-size:large;margin-left:10px' class = 'text-danger md-delete' onclick = '$(this).parent().parent().remove()'></i></td></tr>";
         $("#add_tambahan_beli_but_container").before(html);
         tambahan_beli_row++;        
+    }
+    function empty_table_form(){
+        var brg_beli_row = 0;  
+        var tambahan_beli_row = 0;
+        $(".add_brg_beli_row").remove();
+        $(".add_tambahan_beli_row").remove();
     }
 </script>
