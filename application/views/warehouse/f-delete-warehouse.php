867d573e25b2006d91a6c527d2064448bd31a98b
joshuanatan <joshuanatan.jn@gmail.com>
2020-05-14 22:16:49 +0700

    Update f-delete-warehouse.php

--- application/views/warehouse/f-delete-warehouse.php
+++ application/views/warehouse/f-delete-warehouse.php
@@ -6,9 +6,9 @@
             </div>
             <div class = "modal-body">
                 <form method="POST" action="<?php echo base_url() ?>warehouse/hapus_warehouse">
-                    <input type="hidden" name="id_pk_warehouse" value="<?php echo $view_warehouse[$x]['ID_PK_WAREHOUSE'] ?>"> 
+                    <input type="hidden" name="id_pk_warehouse" value="<?php echo $ID_PK_WAREHOUSE ?>"> 
                     <div class = "form-group">
-                        <h5 style="text-align:center">Apakah anda yakin akan menghapus warehouse dengan nama: "<b><?php echo $view_warehouse[$x]['WAREHOUSE_NAMA'] ?></b>"?</h5>
+                        <h5 style="text-align:center">Apakah anda yakin akan menghapus warehouse dengan nama: "<b><?php echo $WAREHOUSE_NAMA ?></b>"?</h5>
                     </div>
                     <div class = "form-group">
                         <button type = "button" class = "btn btn-sm btn-danger" data-dismiss = "modal">Cancel</button>
