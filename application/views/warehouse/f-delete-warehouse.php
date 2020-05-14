2eb7fd5dd7da06a8777aee5db97515be7eb28f29
joshuanatan <joshuanatan.jn@gmail.com>
2020-05-14 22:08:46 +0700

    warehouse restructure

---
+++ application/views/warehouse/f-delete-warehouse.php
@@ -0,0 +1,21 @@
+<div class = "modal fade" id = "hapus_warehouse<?php echo $x+1 ?>">
+    <div class = "modal-dialog">
+        <div class = "modal-content">
+            <div class = "modal-header">
+                <b><h4 class = "modal-title">Hapus Warehouse</h4></b>
+            </div>
+            <div class = "modal-body">
+                <form method="POST" action="<?php echo base_url() ?>warehouse/hapus_warehouse">
+                    <input type="hidden" name="id_pk_warehouse" value="<?php echo $view_warehouse[$x]['ID_PK_WAREHOUSE'] ?>"> 
+                    <div class = "form-group">
+                        <h5 style="text-align:center">Apakah anda yakin akan menghapus warehouse dengan nama: "<b><?php echo $view_warehouse[$x]['WAREHOUSE_NAMA'] ?></b>"?</h5>
+                    </div>
+                    <div class = "form-group">
+                        <button type = "button" class = "btn btn-sm btn-danger" data-dismiss = "modal">Cancel</button>
+                        <input type = "submit" class = "btn btn-sm btn-primary" value="Yakin">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
