a6dd9ecd8a4e51904d2c56519ab8dc19ff2bad17
wivinadaicy <43501017+wivinadaicy@users.noreply.github.com>
2020-05-18 13:58:01 +0700

    test

--- application/controllers/ws/Barang_warehouse.php
+++ application/controllers/ws/Barang_warehouse.php
@@ -4,4 +4,52 @@ class Barang_warehouse extends CI_Controller{
     public function __construct(){
         parent::__construct();
     }
+
+    public function register(){
+        $response["status"] = "SUCCESS";
+        $checks = $this->input->post("check");
+        if($checks != ""){
+            foreach($checks as $a){
+                $this->form_validation->set_rules("brg".$a,"Barang","required");
+                $this->form_validation->set_rules("brg_warehouse_notes".$a,"Notes","required");
+                $this->form_validation->set_rules("id_fk_Warehouse","Warehouse","required");
+                  
+                if($this->form_validation->run()){
+
+                    $brg = $this->input->post("brg".$a);
+                    $id_fk_brg = get1Value("mstr_barang","id_pk_brg",array("brg_nama"=>$brg));
+                    $brg_warehouse_notes=$this->input->post("brg_warehouse_notes".$a);
+                    $id_fk_warehouse = $this->input->post("id_fk_Warehouse");
+                    $brg_warehouse_status = "AKTIF";
+                    $brg_warehouse_qty=0;
+                    $brg_warehouse_create_date = date("Y-m-d H:i:s");
+                    $brg_warehouse_last_modified = date("Y-m-d H:i:s");
+                    $id_create_data = $this->session->id_user;
+                    $id_last_modified = $this->session->id_user;
+
+                    $data = array(
+                        "id_fk_brg" => $id_fk_brg,
+                        "brg_warehouse_notes" => $brg_warehouse_notes,
+                        "id_fk_warehouse" => $id_fk_warehouse,
+                        "brg_warehouse_status" => $brg_warehouse_status,
+                        "brg_warehouse_qty" => $brg_warehouse_qty,
+                        "brg_warehouse_create_date" => $brg_warehouse_create_date,
+                        "brg_warehouse_last_modified" => $brg_warehouse_last_modified,
+                        "id_create_data" => $id_create_data,
+                        "id_last_modified" => $id_last_modified
+                    );
+                    insertRow("tbl_brg_warehouse",$data);
+                    echo "insert $a";
+                }else{
+                    $response["status"] = "ERROR";
+                    $response["msg"] = validation_errors();
+                    $this->session->set_flashdata("msg",$response['msg']);
+                }
+             }
+             //redirect("warehouse/warehouse_barang/".$id_fk_warehouse);
+             echo json_encode($response);   
+       }else{
+           
+       }
+    }
 }
--- application/views/brg_warehouse/f-add-brg_warehouse.php
+++ application/views/brg_warehouse/f-add-brg_warehouse.php
@@ -6,7 +6,8 @@
                 <h4 class = "modal-title">Tambah Data <?php echo ucwords($page_title);?></h4>
             </div>
             <div class = "modal-body">
-                <form id = "register_form" method = "POST">
+                <form id = "register_form" method = "POST" action="<?php echo base_url()?>warehouse/register_brg_warehouse">
+                    <input type="hidden" name="id_fk_Warehouse" value="<?php echo $warehouse[0]['ID_PK_WAREHOUSE'] ?>" >
                     <div class = "form-group">
                         <h5>Item Pembelian</h5>
                         <table class = "table table-striped table-bordered">
@@ -35,7 +36,7 @@
 <script>
     var brg_count = 0;  
     function add_brg_beli_row(){
-        var html = "<tr class = 'add_brg_count'><td id = 'row"+brg_count+"'><input name = 'check[]' value = "+brg_count+" type = 'hidden'><input type = 'text' list = 'daftar_barang' name = 'brg"+brg_count+"' class = 'form-control'></td><td><input type = 'text' name = 'brg_notes"+brg_count+"' class = 'form-control'></td><td><i style = 'cursor:pointer;font-size:large;margin-left:10px' class = 'text-danger md-delete' onclick = '$(this).parent().parent().remove()'></i></td></tr>";
+        var html = "<tr class = 'add_brg_count'><td id = 'row"+brg_count+"'><input name = 'check[]' value = "+brg_count+" type = 'hidden'><input type = 'text' list = 'daftar_barang' name = 'brg"+brg_count+"' class = 'form-control'></td><td><input type = 'text' name = 'brg_warehouse_notes"+brg_count+"' class = 'form-control'></td><td><i style = 'cursor:pointer;font-size:large;margin-left:10px' class = 'text-danger md-delete' onclick = '$(this).parent().parent().remove()'></i></td></tr>";
         $("#add_brg_beli_but_container").before(html);
         brg_count++;    
     }
