c28fec28a99f5e2cfbb7ca7d7657f2c1fc0a3a0c
joshuanatan <joshuanatan.jn@gmail.com>
2020-05-26 16:36:00 +0700

    Revert "penerimaan"
    This reverts commit 2506ec39c04eb16c95f726bd8c3e7652427bd41a.

--- application/controllers/Penerimaan.php
+++ application/controllers/Penerimaan.php
@@ -12,6 +12,7 @@ class Penerimaan extends CI_Controller{
         $data["type"] = "WAREHOUSE";
         
         $this->load->model("m_pembelian");
+        $this->m_pembelian->set_id_fk_cabang($this->session->id_cabang);
         $data["pembelian"] = $this->m_pembelian->list()->result_array();
         $this->load->view("penerimaan/v_penerimaan",$data);
     }
