4e05147b3213291a08928c7fc8173fe3ed4cd8c6
joshuanatan <joshuanatan.jn@gmail.com>
2020-06-21 23:54:57 +0700

    revisi model
    pindah huruf besar

--- application/models/M_brg_permintaan.php
+++ application/models/M_brg_permintaan.php
@@ -1,7 +1,7 @@
 <?php
 defined("BASEPATH") or exit("no direct script");
 date_default_timezone_set("asia/jakarta");
-class m_brg_permintaan extends ci_model{
+class M_brg_permintaan extends ci_model{
     private $tbl_name = "tbl_brg_permintaan";
     private $columns = array();
     private $id_pk_brg_permintaan;
