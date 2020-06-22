929f019156327af3d51b32cfbb1b9c7477258f4d
joshuanatan <joshuanatan.jn@gmail.com>
2020-06-22 14:27:51 +0700

    Update LOG_ALL.sql

--- query/LOG_ALL.sql
+++ query/LOG_ALL.sql
@@ -1,10 +1,12 @@
 
+drop table if exists log_all;
 create table log_all(
 	id_log_all int primary key auto_increment,
     id_user int,
     log_date datetime,
     log varchar(100)
 );
+drop procedure if exists insert_log_all;
 delimiter $$
 create procedure insert_log_all(in id_user int, in log_date datetime, in log_text varchar(100), out id_log_all int)
 begin
