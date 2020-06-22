54e4f069ce63e49b1c56ef9ede2e19575bd2a21a
joshuanatan <joshuanatan.jn@gmail.com>
2020-06-22 10:09:18 +0700

    Update general.php
    collapsable menu general

--- application/views/req/menu/general.php
+++ application/views/req/menu/general.php
@@ -5,4 +5,14 @@
 <li>
     <hr class="light-grey-hr mb-10" />
 </li>
-<li id = "general_menu_separator"></li>
+<li>
+    <a href="#" href="javascript:void(0);" data-toggle="collapse" data-target="#sub_menu_umum" class="collapsed" ><?php #redirect ke daftar toko yang dia boleh akses?> 
+        <div class="pull-left">
+            <span class="right-nav-text">MANAJEMEN UMUM</span>
+        </div>
+        <hr/>
+    </a>
+    <ul id="sub_menu_umum" class="collapse-level-1 collapse" aria-expanded="false" style="height: 0px;">
+        <li id = "general_menu_separator"></li>
+    </ul>
+</li>
