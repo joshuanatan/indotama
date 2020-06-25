4d723cf384e43579a6f36fecc640a0811db91c3c
joshuanatan <joshuanatan.jn@gmail.com>
2020-06-25 19:53:12 +0700

    Update Pengiriman.php

--- application/controllers/Pengiriman.php
+++ application/controllers/Pengiriman.php
@@ -10,8 +10,22 @@ class Pengiriman extends CI_Controller{
         $this->load->view("pengiriman/v_pengiriman",$data);
     }
     public function cabang(){
+        $this->load->model("m_satuan");
+        $result = $this->m_satuan->list();
+        $data["satuan"] = $result->result_array();
         $data["type"] = "cabang";
         $data["id_tempat_pengiriman"] = $this->session->id_cabang; 
+        $data["tipe_pengiriman"] = "penjualan";
         $this->load->view("pengiriman/v_pengiriman",$data);
+    }
+    public function retur(){
+        $this->load->model("m_satuan");
+        $result = $this->m_satuan->list();
+        $data["satuan"] = $result->result_array();
+        
+        $data["id_tempat_pengiriman"] = $this->session->id_cabang;
+        $data["tipe_pengiriman"] = "retur";
+        $data["type"] = "cabang";
+        $this->load->view("pengiriman_retur/v_pengiriman_retur",$data);
     }
 }
