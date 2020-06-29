ed7225de19e752fa27dadc2f730aeea27d7dfe79
joshuanatan <joshuanatan.jn@gmail.com>
2020-06-29 18:40:47 +0700

    Create datalist_marketplace.php

---
+++ application/views/_base_element/datalist_marketplace.php
@@ -0,0 +1,23 @@
+
+<datalist id = 'datalist_marketplace'></datalist>
+<script>
+    var html_option_marketplace = "";
+    function load_datalist_marketplace(){
+        $.ajax({
+            url:"<?php echo base_url();?>ws/marketplace/list",
+            type:"GET",
+            dataType:"JSON",
+            async:false,
+            success:function(respond){
+                var html_datalist_marketplace = "";
+                if(respond["status"] == "SUCCESS"){
+                    for(var a = 0; a<respond["content"].length; a++){
+                        html_datalist_marketplace+="<option value = '"+respond['content'][a]["nama"]+"'></option>";
+                        html_option_marketplace+="<option value = '"+respond['content'][a]["id"]+"'>"+respond['content'][a]["nama"]+"</option>";
+                    }
+                    $("#datalist_marketplace").html(html_datalist_marketplace);
+                }
+            }
+        });
+    }
+</script>
