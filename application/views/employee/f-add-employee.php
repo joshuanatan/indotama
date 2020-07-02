bcf69ad5e2e8fde2e2256a29716021dd5c3fcb7b
joshuanatan <joshuanatan.jn@gmail.com>
2020-07-02 20:51:46 +0700

    Update f-add-employee.php

--- application/views/employee/f-add-employee.php
+++ application/views/employee/f-add-employee.php
@@ -11,7 +11,19 @@
             );
             $this->load->view('_notification/register_error',$notif_data); ?>
                 <form id = "register_form" method = "POST" enctype = "multipart/form-data">
-                    
+                    <div class = "form-group col-lg-6">
+                        <h5>Panggilan</h5>
+                        <select name="emp_suff" class="form-control">
+                            <option value="0" disabled>Pilih Panggilan</option>
+                            <option value="Tn">Tn</option>
+                            <option value="MR">Mr</option>
+                            <option value="MRS">Mrs</option>
+                            <option value="MS">Ms</option>
+                            <option value="BAPAK">Bpk</option>
+                            <option value="IBU">Ibu</option>
+                            <option value="NONA">Nona</option>
+                        </select>
+                    </div>
                     <div class = "form-group col-lg-6">
                         <h5>Nama Lengkap</h5>
                         <input type="text" class="form-control" name="emp_nama" required>
@@ -32,10 +44,6 @@
                         <input type="text" class="form-control" name="emp_hp" required>
                     </div>
                     
-                    <div class = "form-group col-lg-6">
-                        <h5>Alamat</h5>
-                        <input type="text" class="form-control" name="emp_alamat" required>
-                    </div>
                     
                     <div class = "form-group col-lg-6">
                         <h5>Kode Pos</h5>
@@ -68,42 +76,29 @@
                     </div>
                     
                     <div class = "form-group col-lg-6">
+                        <h5>Rekening Bank</h5>
+                        <input type="text" class="form-control" name="emp_rek" required>
+                    </div>
+                    <div class = "form-group col-lg-12">
                         <h5>Mulai Bekerja</h5>
                         <input type="date" class="form-control" name="emp_startdate" required>
                     </div>
                     
-                    <div class = "form-group col-lg-6">
+                    <div class = "form-group col-lg-12">
                         <h5>Akhir Bekerja</h5>
                         <input type="radio" name="radio_enddate" id="no_enddate" value="MASIH" checked>Masih Bekerja
                         <br><input type="radio" value="TIDAK" name="radio_enddate" id="yes_enddate">Tidak bekerja sejak:
                         <input type="date" style="display:none" class="form-control" id="emp_enddate" name="emp_enddate">
                     </div>
                     
-                    <div class = "form-group col-lg-6">
-                        <h5>Rekening Bank</h5>
-                        <input type="text" class="form-control" name="emp_rek" required>
-                    </div>
                     
-                    <div class = "form-group col-lg-6">
+                    <div class = "form-group col-lg-12">
                         <h5>Jenis Kelamin</h5>
                         <input type="radio" name="emp_gender" value="PRIA" checked>Pria 
                         <input type="radio" name="emp_gender" value="WANITA">Wanita
                     </div>
                     
-                    <div class = "form-group col-lg-6">
-                        <h5>Panggilan</h5>
-                        <select name="emp_suff" class="form-control">
-                            <option value="0" disabled>Pilih Panggilan</option>
-                            <option value="MR">Mr</option>
-                            <option value="MRS">Mrs</option>
-                            <option value="MS">Ms</option>
-                            <option value="BAPAK">Bpk</option>
-                            <option value="IBU">Ibu</option>
-                            <option value="NONA">Nona</option>
-                        </select>
-                    </div>
-                    
-                    <div class = "form-group col-lg-6">
+                    <div class = "form-group col-lg-12">
                         <h5>Toko</h5>
                         <select class="form-control" name="id_fk_toko">
                             <option value="0" disabled>Pilih Toko</option>
@@ -111,6 +106,10 @@
                                 <option value="<?php echo $toko[$p]['id_pk_toko'] ?>"><?php echo $toko[$p]['toko_nama']?></option>
                             <?php } ?>
                         </select>
+                    </div>
+                    <div class = "form-group col-lg-12">
+                        <h5>Alamat</h5>
+                        <textarea class="form-control" name="emp_alamat" required></textarea>
                     </div>
                     <div class = "form-group">
                         <button type = "button" class = "btn btn-sm btn-danger" data-dismiss = "modal">Cancel</button>
