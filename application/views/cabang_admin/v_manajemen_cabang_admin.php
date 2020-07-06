a271b4813c3c18984722ed10c45a3c9376a27576
joshuanatan <joshuanatan.jn@gmail.com>
2020-07-06 09:39:18 +0700

    Create v_manajemen_cabang_admin.php

---
+++ application/views/cabang_admin/v_manajemen_cabang_admin.php
@@ -0,0 +1,104 @@
+<?php
+$page_title = "Admin Cabang";
+$breadcrumb = array(
+    "Master",$toko[0]["toko_nama"],$cabang[0]["cabang_daerah"],"Admin"
+);
+$notif_data = array(
+    "page_title"=>$page_title
+);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <?php $this->load->view('req/mm_css.php');?>
+    </head>
+
+    <body>
+        <div class="preloader-it">
+            <div class="la-anim-1"></div>
+        </div>
+        <div class="wrapper theme-1-active pimary-color-pink">
+
+            <?php $this->load->view('req/mm_menubar');?>
+
+            <div class="page-wrapper">
+            <?php $this->load->view('_notification/register_success',$notif_data); ?>
+            <?php $this->load->view('_notification/update_success',$notif_data); ?>
+            <?php $this->load->view('_notification/delete_success',$notif_data); ?>
+                <div class="container-fluid">
+                    <div class="row mt-20">
+                        <div class="col-lg-12 col-sm-12">
+                            <div class="panel panel-default card-view">
+                                <div class="panel-heading bg-gradient">
+                                    <div class="pull-left">
+                                        <h6 class="panel-title txt-light"><?php echo ucwords($page_title);?></h6>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item">Home</a></li>
+                                        <?php for($a = 0; $a<count($breadcrumb); $a++):?>
+                                        <?php if($a+1 != count($breadcrumb)):?>
+                                        <li class="breadcrumb-item"><?php echo ucwords($breadcrumb[$a]);?></a></li>
+                                        <?php else:?>
+                                        <li class="breadcrumb-item active"><?php echo ucwords($breadcrumb[$a]);?></li>
+                                        <?php endif;?>
+                                        <?php endfor;?>
+                                    </ol>
+                                </div>
+                                <div class="panel-wrapper collapse in">
+                                    <div class="panel-body">
+                                        <div class = "col-lg-12">
+                                            <div class = "d-block">
+                                                <a href = "<?php echo base_url();?>toko/cabang_toko" style = "margin-right:10px" class = "btn btn-danger btn-sm col-lg-2 col-sm-12">Kembali ke Daftar Cabang</a>
+                                                <button type = "button" class = "btn btn-primary btn-sm col-lg-2 col-sm-12" data-toggle = "modal" data-target = "#register_modal" style = "margin-right:10px">Tambah <?php echo ucwords($page_title);?></button>
+                                            </div>
+                                            <br/>
+                                            <br/>
+                                            <div class = "align-middle text-center d-block">
+                                                <i style = "cursor:pointer;font-size:large;margin-left:10px" class = "text-primary md-edit"></i><b> - Edit </b>   
+                                                <i style = "cursor:pointer;font-size:large;margin-left:10px" class = "text-danger md-delete"></i><b> - Delete </b>
+                                               </div>
+                                            <br/>
+                                            <?php $this->load->view("_base_element/table");?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php $this->load->view('req/mm_footer.php');?>
+                </div>
+            </div>
+        </div>
+        <?php $this->load->view('req/mm_js.php');?>
+    </body>
+</html>
+<script>
+    var ctrl = "cabang_admin";
+    var url_add = "id_cabang=<?php echo $id_cabang;?>";
+</script>
+<?php 
+$data = array(
+    "page_title" => "Admin Toko",
+    "cabang" => $cabang
+);
+?>
+<?php $this->load->view("_core_script/table_func");?>
+<?php $this->load->view("_core_script/register_func");?>
+<?php $this->load->view("_core_script/update_func");?>
+<?php $this->load->view("_core_script/delete_func");?>
+<?php $this->load->view('cabang_admin/f-add-cabang_admin',$data);?>
+<?php $this->load->view('cabang_admin/f-update-cabang_admin',$data);?>
+<?php $this->load->view('cabang_admin/f-delete-cabang_admin',$data);?>
+
+
+
+<?php $this->load->view('_notification/notif_general'); ?>
+
+<?php $this->load->view("_base_element/datalist_user");?>
+<script>
+    function load_datalist(){
+        load_datalist_user();
+    }
+</script>
