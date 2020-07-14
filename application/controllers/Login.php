8effdd073d516a9cedfefe7c0573789a47d63a2b
wivinadaicy <wivinadaicy.wd@gmail.com>
2020-07-14 19:51:03 +0700

    slide 3 forget password

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -53,74 +53,68 @@ class Login extends CI_Controller {
 
 	public function forget_password_method(){
 		$response["status"] = "SUCCESS";
-		$this->form_validation->set_rules("user_name","Username","required");
+		$this->form_validation->set_rules("user_email","Email","required|valid_email");
 
-		$user_name = $this->input->post("user_name");
+		$user_email = $this->input->post("user_email");
 		if($this->form_validation->run()){
 			$where = array(
 				"user_status" => "AKTIF",
-				"user_name"=>$user_name
+				"user_email"=>$user_email
 			);
 			if(isExistsInTable("mstr_user",$where)){
-				$where = array(
-					"user_status" => "AKTIF",
-					"user_name" =>$user_name
-				);
-				if(isExistsInTable("mstr_user",$where)){
-					$data['user_email'] = get1Value("mstr_user","user_email",$where);
-					$data['id_pk_user'] = get1Value("mstr_user","id_pk_user",$where);
-					//send email
-					$this->load->library('phpmailer_lib');
-					$mail = $this->phpmailer_lib->load();
-					$mail->IsSMTP();
+				$data['id_pk_user'] = get1Value("mstr_user","id_pk_user",$where);
+				$data['user_name'] = get1Value("mstr_user","user_name",$where);
+				$data['user_pass'] = get1Value("mstr_user","user_pass",$where);
+				
+				//send email
+				$this->load->library('phpmailer_lib');
+				$mail = $this->phpmailer_lib->load();
+				$mail->IsSMTP();
 
-					$mail->Host       = "smtp.gmail.com";
-					$mail->SMTPSecure = "ssl";
-					$mail->Port       = 465;
-					$mail->Username   = "clickrentsistech@gmail.com";
-					$mail->Password   = "sistech123";
+				$mail->Host       = "smtp.gmail.com";
+				$mail->SMTPSecure = "ssl";
+				$mail->Port       = 465;
+				$mail->Username   = "clickrentsistech@gmail.com";
+				$mail->Password   = "sistech123";
 
-					$mail->SMTPAuth   = true;
-					$mail->SetFrom('clickrentsistech@gmail.com', 'INDOTAMA');
-					$mail->AddAddress($data['user_email']);
-					$mail->Subject    = "Reset Your Password";
-					$mail->IsHTML(true);
+				$mail->SMTPAuth   = true;
+				$mail->SetFrom('clickrentsistech@gmail.com', 'INDOTAMA');
+				$mail->AddAddress($user_email);
+				$mail->Subject    = "Reset Your Password";
+				$mail->IsHTML(true);
 
-					$isiEmail = "<center style='color:black'>LOGO<br>
-					<h2>Reset Password</h2>
-					<p>Hi<br>Reset your password by clicking <a href='".base_url()."Login/forget_get_new_pass/".$data['id_pk_user']."'>HERE</a></center>
-					";
-					$mail->Body       = $isiEmail;
-		
-					//$mail->SMTPDebug = 2;
+				$link_reset = base_url() . 'Login/forget_get_new_pass/' . md5($data['id_pk_user']) . '/' . $data['user_pass'];
 
-					if(!$mail->Send()) {
-						//echo "Mailer Error: " . $mail->ErrorInfo;
-						echo "<script>alert('Gagal reset password! Silahkan hubungi admin sistem!'); window.location.href='../99dea78007133396a7b8ed70578ac6ae';</script>";
-					} 
-					else {
-						echo "<script>alert('Cek email Anda dan ikuti instruksi selanjutnya untuk melakukan reset password!'); window.location.href='../99dea78007133396a7b8ed70578ac6ae';</script>";
-					}
-				}
-				else{
+				$isiEmail = "<center style='color:black'>LOGO<br>
+				<h2>Reset Password</h2>
+				<p>Hi<br>Reset your password by clicking <a href='".$link_reset."'>HERE</a></center>
+				";
+				$mail->Body       = $isiEmail;
+	
+				//$mail->SMTPDebug = 2;
+
+				if(!$mail->Send()) {
+					//echo "Mailer Error: " . $mail->ErrorInfo;
 					$response["status"] = "ERROR";
-					$response["msg"] = "Employee tidak ditemukan!";
-					//redirect("login/forget_password");
+					$response["msg"] = "Gagal reset password! Silahkan hubungi admin sistem!";
+					$this->session->set_flashdata("msg",$response['msg']);
+				} 
+				else {
+					$response["msg"] = "Cek email Anda dan ikuti instruksi selanjutnya untuk melakukan reset password!";
+					$this->session->set_flashdata("success_send",$response['msg']);
 				}
 			}
 			else{
 				$response["status"] = "ERROR";
-				$response["msg"] = "Employee tidak ditemukan!!";
-				//redirect("login/forget_password");
+				$response["msg"] = "User tidak ditemukan!";
 			}
 		}
 		else{
 			$response["status"] = "ERROR";
 			$response["msg"] = validation_errors();
 			$this->session->set_flashdata("msg",$response['msg']);
-			//redirect("login/forget_password");
 		}
-		echo $response['msg'];
+		redirect("99dea78007133396a7b8ed70578ac6ae");
 	}
 
 	public function change_password(){
@@ -146,8 +140,24 @@ class Login extends CI_Controller {
 		}
 	}
 
-	public function forget_get_new_pass($id_pk_user){
-		$data['id_pk_user'] = $id_pk_user;
+	public function forget_get_new_pass($md5_id,$md5){
+		$where = array(
+			"user_pass"=>$md5
+		);
+		$admin = selectRow("mstr_user",$where)->result_array();
+
+		if(count($admin)==0){
+			$this->session->set_flashdata("msg","Your link has been expired!");
+			redirect("99dea78007133396a7b8ed70578ac6ae");
+		}else if(count($admin)==1){
+			$data['id_pk_user'] = $admin[0]['id_pk_user'];
+		}else{
+			for($x=0; $x<count($admin); $x++){
+				if(md5($admin[$x]['id_pk_user'])==$md5_id){
+					$data['id_pk_user'] = $admin[$x]['id_pk_user'];
+				}
+			}
+		}
 		$this->load->view('login/V_reset_password',$data);
 	}
 
@@ -183,8 +193,8 @@ class Login extends CI_Controller {
                 "id_pk_user"=>$this->input->post('id_pk_user')
             );
             updateRow("mstr_user",$data,$where);
-
-			echo "<script>alert('Password telah diperbarui! Silahkan login'); window.location.href='../99dea78007133396a7b8ed70578ac6ae';</script>";
+			$this->session->set_flashdata("status","success");
+			redirect("99dea78007133396a7b8ed70578ac6ae");
             
         }
         else{
