7f09a860c264ae32b0fcb62af9d0afc2a5e1edc1
wivinadaicy <wivinadaicy.wd@gmail.com>
2020-07-14 22:02:17 +0700

    Update error_404.php

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -1,64 +1,81 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<?php $base_url = "http://localhost/indotama"; ?>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<title>404 Page Not Found</title>
-<style type="text/css">
-
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
-
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
-</head>
-<body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
-</body>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<title>INDOTAMA</title>
+		
+		<!-- vector map CSS -->
+		<link href="<?php echo $base_url; ?>/vendors/bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.min.css" rel="stylesheet" type="text/css"/>
+		
+		
+		
+		<!-- Custom CSS -->
+		<link href="<?php echo $base_url; ?>/asset/dist/css/style.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<!--Preloader-->
+		<div class="preloader-it">
+			<div class="la-anim-1"></div>
+		</div>
+		<!--/Preloader-->
+		
+		<div class="wrapper error-page pa-0">
+			<header class="sp-header">
+				<div class="sp-logo-wrap pull-left">
+					<a href="index.html">
+						<!--<img class="brand-img mr-10" src="../img/logo.png" alt="brand"/>-->
+						<span class="brand-text">INDOTAMA</span>
+					</a>
+				</div>
+				<div class="form-group mb-0 pull-right">
+					<a class="inline-block btn btn-primary btn-rounded btn-outline nonecase-font" href="<?php echo $base_url; ?>/2938c7f7e560ed972f8a4f68e80ff834">Back to Home</a>
+				</div>
+				<div class="clearfix"></div>
+			</header>
+			
+			<!-- Main Content -->
+			<div class="page-wrapper pa-0 ma-0 error-bg-img">
+				<div class="container-fluid">
+					<!-- Row -->
+					<div class="table-struct full-width full-height">
+						<div class="table-cell vertical-align-middle auth-form-wrap">
+							<div class="auth-form  ml-auto mr-auto no-float">
+								<div class="row">
+									<div class="col-sm-12 col-xs-12">
+										<div class="mb-30">
+											<span class="block error-head text-center txt-primary mb-10">404</span>
+											<span class="text-center nonecase-font mb-20 block error-comment">Page Not Found</span>
+											<p class="text-center">The URL may be misplaced or the pahe you are looking is no longer available.</p>
+										</div>	
+									</div>	
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- /Row -->	
+				</div>
+				
+			</div>
+			<!-- /Main Content -->
+		
+		</div>
+		<!-- /#wrapper -->
+		
+		<!-- JavaScript -->
+		
+		<!-- jQuery -->
+		<script src="<?php echo $base_url; ?>/vendors/bower_components/jquery/dist/jquery.min.js"></script>
+		
+		<!-- Bootstrap Core JavaScript -->
+		<script src="<?php echo $base_url; ?>/vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="<?php echo $base_url; ?>/vendors/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.min.js"></script>
+		
+		<!-- Slimscroll JavaScript -->
+		<script src="<?php echo $base_url; ?>/asset/dist/js/jquery.slimscroll.js"></script>
+		
+		<!-- Init JavaScript -->
+		<script src="<?php echo $base_url; ?>/asset/dist/js/init.js"></script>
+	</body>
 </html>
