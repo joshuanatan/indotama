c53e4861c15a7c007586eaf22a4281be4977024f
joshuanatan <joshuanatan.jn@gmail.com>
2020-07-31 21:46:50 +0700

    Update f-update-marketplace.php

--- application/views/marketplace/f-update-marketplace.php
+++ application/views/marketplace/f-update-marketplace.php
@@ -16,16 +16,16 @@
                     
                     <div class = "form-group">
                         <h5>Nama Marketplace</h5>
-                        <input type = "text" class = "form-control" required id = "nama_edit">
+                        <input type = "text" class = "form-control" required name = "nama" id = "nama_edit">
                     </div>
                     
                     <div class = "form-group">
                         <h5>Keterangan</h5>
-                        <input type = "text" class = "form-control" required id = "keterangan_edit">
+                        <input type = "text" class = "form-control" required name = "keterangan" id = "keterangan_edit">
                     </div>
                     <div class = "form-group">
                         <h5>Biaya</h5>
-                        <input type = "text" class = "form-control nf-input" required id = "biaya_edit">
+                        <input type = "text" class = "form-control nf-input" required name = "biaya" id = "biaya_edit">
                     </div>
                     <div class = "form-group">
                         <button type = "button" class = "btn btn-sm btn-danger" data-dismiss = "modal">Cancel</button>
